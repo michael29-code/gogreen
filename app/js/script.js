90cc4076276fa118b7c60fe88714c336d3c6a3cc
Michael <alfonsusmichael2003@gmail.com>
2021-04-02 10:17:46 +0700

    add our program
    add new section using grid

--- app/js/script.js
+++ app/js/script.js
@@ -2,37 +2,36 @@ const btnHamburger = document.querySelector('#btnHamburger');
 const body = document.querySelector('body');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
-const fadeElems =document.querySelectorAll('.has-fade');
-const backward = document.querySelector('svg');
+const fadeElems = document.querySelectorAll('.has-fade');
 
 
-btnHamburger.addEventListener('click',function(){
+btnHamburger.addEventListener('click', function () {
     console.log('open hamburger');
 
-    if(header.classList.contains('open')){ //cllose burger
+    if (header.classList.contains('open')) { //cllose burger
         header.classList.remove('open');
         body.classList.remove('noscroll');
-        fadeElems.forEach(function(element) {
+        fadeElems.forEach(function (element) {
             element.classList.remove('fade-in');
             element.classList.add('fade-out');
-            });
-    }else{
+        });
+    } else {
         body.classList.add('noscroll');
         header.classList.add('open');//open
-        fadeElems.forEach(function(element) {
-        element.classList.remove('fade-out');
-        element.classList.add('fade-in');
+        fadeElems.forEach(function (element) {
+            element.classList.remove('fade-out');
+            element.classList.add('fade-in');
         });
-       
-    }
-} );
 
-overlay.addEventListener('click',function(){
+    }
+});
+
+overlay.addEventListener('click', function () {
     header.classList.remove('open');
-        body.classList.remove('noscroll');
-        fadeElems.forEach(function(element) {
-            element.classList.remove('fade-in');
-            element.classList.add('fade-out');
+    body.classList.remove('noscroll');
+    fadeElems.forEach(function (element) {
+        element.classList.remove('fade-in');
+        element.classList.add('fade-out');
     });
 });
 
@@ -40,12 +39,12 @@ overlay.addEventListener('click',function(){
 // animation
 var animation = bodymovin.loadAnimation({
     container: document.querySelector('.hero__image'),
-        
+
     // Set your ID to something that you'll associate with the animation you're using //
     renderer: 'svg',
     loop: true,
     autoplay: true,
     path: 'asset/hero.json'
-        
-    // Make sure your path has the same filename as your animated     SVG's JSON file //
- });
+
+    // Make sure your path has the same filename as your animated SVG's JSON file //
+});
